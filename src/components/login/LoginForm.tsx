2f9e5b54d906099aaf38fe6138f3afad24343dbc
NoamAnisfeld <NoamAnisfeld@gmail.com>
2023-01-30 14:23:08 +0200

    require correct credentials on login

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,18 +1,23 @@
 import '../../styles/LoginPage.scss';
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { validateCredentials } from '../../mockup-backend/api';
 import { login, useAppSelector, useAppDispatch } from "../../store/store";
 
 export default function LoginForm() {
-    const loggedInUser = useAppSelector(state => state.user);
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginFailed, setLoginFailed] = useState(false);
+
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
-    function performLogin() {
-        if (username) {
+    async function performLogin() {
+        if (await validateCredentials(username, password)) {
             dispatch(login(username));
             navigate('/');
+        } else {
+            setLoginFailed(true);
         }
     }
 
@@ -27,9 +32,22 @@ export default function LoginForm() {
         </label>
 
         <label>Password
-            <input type="password" />
+            <input type="password"
+                onChange={e => setPassword(e.target.value)}
+            />
         </label>
 
-        <button type="button" onClick={performLogin}>Login</button>
+        {loginFailed ? <p>
+            User not found or credentials don't match, 
+            please try again or register as a new user
+        </p> : undefined}
+
+        <button
+            type="button"
+            onClick={performLogin}
+            disabled={!username || !password}
+        >
+            Login
+        </button>
     </form>
 }
